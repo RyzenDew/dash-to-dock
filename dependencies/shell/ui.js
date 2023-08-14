0c63908d000b42c433d38dd933a1875c8805db59
Marco Trevisan (Treviño) <mail@3v1n0.net>
2023-08-14 15:50:40 +0200

    extension: Port to ESM
    Use ESM imports as gnome-45 requires, so drop compatibility with all the
    old versions so that we could modernize things a bit in future.
    
    Closes: #2066

---
+++ dependencies/shell/ui.js
@@ -0,0 +1,37 @@
+import * as AppDisplay from 'resource:///org/gnome/shell/ui/appDisplay.js';
+import * as AppFavorites from 'resource:///org/gnome/shell/ui/appFavorites.js';
+import * as BoxPointer from 'resource:///org/gnome/shell/ui/boxpointer.js';
+import * as DND from 'resource:///org/gnome/shell/ui/dnd.js';
+import * as Dash from 'resource:///org/gnome/shell/ui/dash.js';
+import * as Layout from 'resource:///org/gnome/shell/ui/layout.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as Overview from 'resource:///org/gnome/shell/ui/overview.js';
+import * as OverviewControls from 'resource:///org/gnome/shell/ui/overviewControls.js';
+import * as PointerWatcher from 'resource:///org/gnome/shell/ui/pointerWatcher.js';
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
+import * as SearchController from 'resource:///org/gnome/shell/ui/searchController.js';
+import * as ShellMountOperation from 'resource:///org/gnome/shell/ui/shellMountOperation.js';
+import * as Workspace from 'resource:///org/gnome/shell/ui/workspace.js';
+import * as WorkspaceSwitcherPopup from 'resource:///org/gnome/shell/ui/workspaceSwitcherPopup.js';
+import * as WorkspaceThumbnail from 'resource:///org/gnome/shell/ui/workspaceThumbnail.js';
+import * as WorkspacesView from 'resource:///org/gnome/shell/ui/workspacesView.js';
+
+export {
+    AppDisplay,
+    AppFavorites,
+    BoxPointer,
+    DND,
+    Dash,
+    Layout,
+    Main,
+    Overview,
+    OverviewControls,
+    PointerWatcher,
+    PopupMenu,
+    SearchController,
+    ShellMountOperation,
+    Workspace,
+    WorkspaceSwitcherPopup,
+    WorkspaceThumbnail,
+    WorkspacesView
+};
